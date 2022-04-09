e0ce7a63599bb75a3bf9ea2c1bf9e811927b0512
LilyC99 <lilyconcepcion123@gmail.com>
2022-04-08 22:54:06 -0400

    commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,12 +10,12 @@ const Footer = () => {
       <LinkList>
         <LinkColumn>
           <LinkTitle>Call</LinkTitle>
-          <LinkItem href="tel:314-343-3432">314-343-3432</LinkItem>
+          <LinkItem href="tel:314-343-3432">201-428-4066</LinkItem>
         </LinkColumn>
         <LinkColumn>
           <LinkTitle>Email</LinkTitle>
-          <LinkItem href="mailto:contact@jsmastery.com">
-            contact@jsmastery.com
+          <LinkItem href="mailto:lilyconcpecion123@gmail.com">
+          lilyconcpecion123@gmail.com
           </LinkItem>
         </LinkColumn>
       </LinkList>
